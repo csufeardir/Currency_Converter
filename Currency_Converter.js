dad9d330b81119bc4dd34f419505c9579940e6a8
Onur Tekin <30865800+csufeardir@users.noreply.github.com>
2019-03-28 11:30:09 +0100

    Fix of calculation errors.
    Errors due to different unit values are fixed.

--- Currency_Converter.js
+++ Currency_Converter.js
@@ -3,6 +3,7 @@ var x;
 var y;
 var amount;
 var convertion;
+var unit;
 
 fetch('https://homel.vsb.cz/~mor03/TAMZ/cnb_json.php')
   .then(response => {
@@ -11,7 +12,7 @@ fetch('https://homel.vsb.cz/~mor03/TAMZ/cnb_json.php')
   .then(data => {
   for (var i=0; i<data.data.length; i++) {
   localStorage.setItem(data.data[i].code, JSON.stringify(data.data[i].rate));
-  
+  localStorage.setItem(data.data[i].code+"_unit", JSON.stringify(data.data[i].unit));
 }
   })
   .catch(err => {
@@ -23,15 +24,15 @@ fetch('https://homel.vsb.cz/~mor03/TAMZ/cnb_json.php')
 		y = 'CZK'; // To CZK
 		amount = 1; // Quantity
 		var rate = JSON.parse(localStorage[x]);
-		
+		unit = JSON.parse(localStorage[x+"_unit"]);
 		
 		if(y == 'CZK'){
-			var res = amount + " " + x + " Equals to " + ((amount)*(parseFloat(rate))).toFixed(2) + " CZK"
+			var res = amount*unit + " " + x + " Equals to " + ((amount)*(parseFloat(rate))).toFixed(2) + " CZK"
 			console.log(res);
 		}
 		else {
 			var rate2 = JSON.parse(localStorage[y]);
-			var res = amount + " " + x + " Equals to " + ((amount)*(parseFloat(rate))/(parseFloat(rate2))).toFixed(2) + " " + y
+			var res = amount*unit + " " + x + " Equals to " + ((amount)*(parseFloat(rate))/(parseFloat(rate2))).toFixed(2) + " " + y
 			console.log(res);
 		}
 		
